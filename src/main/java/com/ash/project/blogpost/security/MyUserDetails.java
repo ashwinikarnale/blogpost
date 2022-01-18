a96eb898dadcd693fd1074ec6279909e209bea54
ashwinikarnale <ashwini.karnale.18@mountblue.tech>
2022-01-18 15:14:19 +0530

    blogpost changes

--- src/main/java/com/ash/project/blogpost/security/MyUserDetails.java
+++ src/main/java/com/ash/project/blogpost/security/MyUserDetails.java
@@ -6,6 +6,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class MyUserDetails implements UserDetails {
@@ -18,7 +19,7 @@ public class MyUserDetails implements UserDetails {
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority(user.getRole());
-        return List.of(simpleGrantedAuthority);
+        return Collections.singleton( new SimpleGrantedAuthority(user.getRole()));
     }
 
     @Override
